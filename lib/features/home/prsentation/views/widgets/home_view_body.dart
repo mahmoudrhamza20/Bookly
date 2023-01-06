f356c11d5f2b557d83d30900031dbd8b8e032d64
Hamza <mahmoudrhamza20@gmail.com>
2023-01-06 18:42:23 +0200

    best seller list view item part 2

--- lib/features/home/prsentation/views/widgets/home_view_body.dart
+++ lib/features/home/prsentation/views/widgets/home_view_body.dart
@@ -23,6 +23,9 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.textStyle18,
           ),
+          SizedBox(
+            height: 20,
+          ),
           BestSellerListViewItem()
         ],
       ),
@@ -36,6 +39,7 @@ class BestSellerListViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         SizedBox(
           height: 120,
@@ -53,6 +57,20 @@ class BestSellerListViewItem extends StatelessWidget {
                   )),
             ),
           ),
+        ),
+       const SizedBox(width: 30,),
+        Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .5,
+              child:const  Text(
+                'Harry Potter and the Goblet of Fire',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: Styles.textStyle20,
+              ),
+            ),
+          ],
         )
       ],
     );
