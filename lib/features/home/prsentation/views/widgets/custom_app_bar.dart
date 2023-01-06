5ceb5a2044756ed72dade75452a3179e536310d7
Hamza <mahmoudrhamza20@gmail.com>
2023-01-06 15:26:07 +0200

    add custom app bar

---
+++ lib/features/home/prsentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(AssetsData.logo,height: 20,),
+          const Spacer(),
+          IconButton(onPressed: (){}, icon:const Icon(Icons.search,size: 24,))
+        ],
+      ),
+    );
+  }
+}
