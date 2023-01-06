18e2c944de5a96231387265c4792e88e684ac575
Hamza <mahmoudrhamza20@gmail.com>
2023-01-06 16:00:19 +0200

    add featured books list view

--- lib/features/home/prsentation/views/widgets/home_view_body.dart
+++ lib/features/home/prsentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
@@ -10,10 +10,12 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: const[
         CustomAppBar(),
-        CustomListViewItem()
+        FeaturedBooksListView()
       ],
     );
   }
 }
+
+
 
 
