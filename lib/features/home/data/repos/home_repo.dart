838679f815e0b205fd2e228bc026c17fe30756d8
Hamza <mahmoudrhamza20@gmail.com>
2023-01-09 14:21:44 +0200

    add book model & create home repo methods & failure class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
 //بعمل abstract class وبحط فيه ال ميثودز الي مفروض هيتم تنفيذها ف الاسكرينة الخاصه ب ال الريبو دا بدون implementation
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo{
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+ Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+ Future<Either<Failure,List<BookModel>>>  fetchFeaturedBooks();
 }
