e15b244bbbac71ea974f72662c4008d63ed5fe95
Hamza <mahmoudrhamza20@gmail.com>
2023-01-09 13:22:23 +0200

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,5 @@
+//بعمل abstract class وبحط فيه ال ميثودز الي مفروض هيتم تنفيذها ف الاسكرينة الخاصه ب ال الريبو دا بدون implementation
+abstract class HomeRepo{
+  fetchBestSellerBooks();
+  fetchFeaturedBooks();
+}
