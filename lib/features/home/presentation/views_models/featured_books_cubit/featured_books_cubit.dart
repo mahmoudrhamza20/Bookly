68b4987f21b3368df25c9243052c185ab0944563
Hamza <mahmoudrhamza20@gmail.com>
2023-01-10 13:59:30 +0200

    Create featured books cubit states

---
+++ lib/features/home/presentation/views_models/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../../../data/models/book_model/book_model.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
